fb8f39d07e73b3937874732bc9251bf4ca1f5a7d
Bruno <kurwee@gmail.com>
2025-02-10 18:43:04 -0300

    push to upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { NavBar } from '@/components/nav-bar'
 import { Hero } from '@/components/hero'
 import { Benefits } from '@/components/benefits'
 import { HowItWorks } from '@/components/how-it-works'
-import { Stats } from '@/components/stats2'
+
 import { Problems } from '@/components/problems'
 import { Footer } from '@/components/footer'
 import DataTypes from '@/components/data-types'
